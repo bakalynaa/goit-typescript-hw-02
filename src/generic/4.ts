d4e2bf92a99aa775a8bdde27354dc5cbad49ba59
Yaroslav <bakalyna.yaroslav@gmail.com>
2023-11-09 13:51:57 +0200

    finish

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface PageTitle {
+  title: string;
+  pageInfo: () => void;
+}
+
+class Component <T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<PageTitle> {
   pageInfo () {
     console.log(this.props.title);
   }
--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,10 +10,13 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
+
+type RoleType = Record<UserRole, string>
+
 const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
 
 export {};
